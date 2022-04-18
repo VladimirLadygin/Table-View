dd080998967eabf62acddb909f7988c39adbf653
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-04-18 19:43:41 +0300

    Add checking for strings from empty values from text fields.

--- Table View/View Controllers/AddEditTableViewController.swift
+++ Table View/View Controllers/AddEditTableViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 // MARK: - Outlets
-class AddEditTableViewController: UITableViewController {
+class AddEditTableViewController: UITableViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var symbolTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var usageTextField: UITextField!
+        
+    @IBOutlet weak var exitButton: UIBarButtonItem!
+    @IBOutlet weak var titleLabel: UINavigationItem!
     
     // MARK: - PROPERTIES
     var emoji = Emoji()
@@ -27,6 +31,7 @@ class AddEditTableViewController: UITableViewController {
         nameTextField.text = emoji.name
         descriptionTextField.text = emoji.description
         usageTextField.text = emoji.usage
+        titleNameUpdate()
     }
     
     func saveEmoji() {
@@ -35,10 +40,37 @@ class AddEditTableViewController: UITableViewController {
         emoji.description = descriptionTextField.text ?? ""
         emoji.usage = usageTextField.text ?? ""
     }
+    
+    func checkSaveButton() {
+        let symbol = symbolTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        let usage = usageTextField.text ?? ""
+        
+        
+        if
+            symbol.count == 1 && symbol.unicodeScalars.first!.properties.isEmoji && name !=  "" && description != "" && usage != ""
+        {
+            exitButton.isEnabled = true
+        } else {
+            exitButton.isEnabled = false
+        }
+    }
+    
+    func titleNameUpdate() {
+        if emoji.symbol != "" && emoji.name != "" && emoji.description != "" && emoji.usage != ""
+        {
+            titleLabel.title = "Edit"
+        }
+    }
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         saveEmoji()
     }
-}
+    
+    @IBAction func textFieldChanged(_ sender: UITextField) {
 
-  
+        checkSaveButton()
+    }
+    
+}
